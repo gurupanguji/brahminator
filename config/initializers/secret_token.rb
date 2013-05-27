ae1645fc9cd8e8efb9c765e179975e0b39bbed94
Renganathan Ramamoorthy <gurupanguji@gmail.com>
2013-05-26 21:59:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brahminator::Application.config.secret_key_base = '134e99d10d054658ab6f2d1c119eb2a3686084c30d5f65cdf932b6e55ddd9d4b416bde8112532d7678d74736710afeb0d626aca5b78ea939512b91e827ec2fcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brahminator::Application.config.session_store :cookie_store, key: '_brahminator_session'
